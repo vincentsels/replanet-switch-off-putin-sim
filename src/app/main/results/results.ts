9970e03384b261bca7be1f4d0a9e98719ac69afd
Vincent Sels <vincent_sels@hotmail.com>
2022-05-13 17:52:00 +0200

    Change bg color putin

--- src/app/main/results/results.ts
+++ src/app/main/results/results.ts
@@ -6,13 +6,13 @@ export class Results {
   }
 
   static moneyImageMap = [
-    { threshold: 17000000000, image: '/assets/img/putin/7_putin_crying.jpg' },
-    { threshold: 20000000000, image: '/assets/img/putin/6_putin_sad.jpg' },
-    { threshold: 25000000000, image: '/assets/img/putin/5_putin_not_amused.jpg' },
-    { threshold: 30000000000, image: '/assets/img/putin/4_putin_worried.jpg' },
-    { threshold: 35000000000, image: '/assets/img/putin/3_putin_smile.jpg' },
-    { threshold: 40000000000, image: '/assets/img/putin/2_putin_big_smile.jpg' },
-    { threshold: 99999999999999, image: '/assets/img/putin/1_putin_toast.jpg' }
+    { threshold: 17000000000, image: './assets/img/putin/7_putin_crying.jpg' },
+    { threshold: 20000000000, image: './assets/img/putin/6_putin_sad.jpg' },
+    { threshold: 25000000000, image: './assets/img/putin/5_putin_not_amused.jpg' },
+    { threshold: 30000000000, image: './assets/img/putin/4_putin_worried.jpg' },
+    { threshold: 35000000000, image: './assets/img/putin/3_putin_smile.jpg' },
+    { threshold: 40000000000, image: './assets/img/putin/2_putin_big_smile.jpg' },
+    { threshold: 99999999999999, image: './assets/img/putin/1_putin_toast.jpg' }
   ]
 
   static gasGapBcm = 155; // Billion cubic meters per year
